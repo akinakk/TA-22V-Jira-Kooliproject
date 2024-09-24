370d1ffea209399d7031c67c8e2e06e6b29c5aff
Akinakk <Artemi.Baburin@tptlive.ee>
2024-09-24 18:26:18 +0300

    feat: feat: store courses in DB

--- feedback-backend/controllers/course_controller.go
+++ feedback-backend/controllers/course_controller.go
@@ -4,13 +4,20 @@ import (
 	"encoding/json"
 	"feedback-backend/models"
 	"feedback-backend/services"
+	"feedback-backend/validators"
+	"log"
 	"net/http"
 )
 
 func GetCourses(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	courses := services.GetCourses()
+	courses, err := services.GetCourses()
+	if err != nil {
+		http.Error(w, "Failed to retrieve courses", http.StatusInternalServerError)
+		log.Printf("Error in GetCourses controller: %v", err)
+		return
+	}
 
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(courses)
@@ -25,9 +32,16 @@ func CreateCourse(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	services.CreateCourse(course)
+	if err := validators.Validate.Struct(course); err != nil {
+		http.Error(w, "Validation failed: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := services.CreateCourse(course); err != nil {
+		http.Error(w, "Failed to create a course", http.StatusInternalServerError)
+		return
+	}
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(course)
 }
-
--- feedback-backend/services/course_service.go
+++ feedback-backend/services/course_service.go
@@ -1,15 +1,45 @@
 package services
 
 import (
+	"feedback-backend/db"
 	"feedback-backend/models"
+	"log"
 )
 
-var courses []models.Course
+func GetCourses() ([]models.Course, error) {
+	query := `SELECT id, course_name FROM courses`
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		log.Printf("Error fetching courses: %v", err)
+		return nil, err
+	}
+	defer rows.Close()
 
-func GetCourses() []models.Course {
-	return courses
+	var courses []models.Course
+	for rows.Next() {
+		var course models.Course
+		if err := rows.Scan(&course.ID, &course.CourseName); err != nil {
+			log.Printf("Error scanning course: %v", err)
+			return nil, err
+		}
+		courses = append(courses, course)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Printf("Rows error: %v", err)
+		return nil, err
+	}
+
+	return courses, nil
 }
 
-func CreateCourse(course models.Course) {
-    courses = append(courses, course)
+func CreateCourse(course models.Course) error {
+	query := "INSERT INTO courses (course_name) VALUES ($1)"
+	_, err := db.DB.Exec(query, course.CourseName)
+	if err != nil {
+		log.Printf("Error inserting student: %v", err)
+		return err
+	}
+
+	return nil
 }
