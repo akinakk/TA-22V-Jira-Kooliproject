c13fcb110dc60da6c229bef9c0ed69052dcdd6c1
Akinakk <Artemi.Baburin@tptlive.ee>
2024-09-26 09:37:51 +0300

    fix: resolve cors issue

--- feedback-backend/main.go
+++ feedback-backend/main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "feedback-backend/db" 
-    "feedback-backend/routes"
+	"feedback-backend/db"
+	"feedback-backend/routes"
+    "feedback-backend/middleware"
+	"log"
+	"net/http"
 )
 
 func main() {
-    db.ConnectDB()
-    router := routes.SetupRoutes()
+	db.ConnectDB()
+	router := routes.SetupRoutes()
 
-    port := ":8080" 
-    log.Printf("The server is running on port %s", port)
-    if err := http.ListenAndServe(port, router); err != nil {
+	port := ":8080"
+	log.Printf("The server is running on port %s", port)
+    
+    if err := http.ListenAndServe(port, middleware.CORS(router)); err != nil {
         log.Fatalf("Error starting the server: %v", err)
     }
 }
