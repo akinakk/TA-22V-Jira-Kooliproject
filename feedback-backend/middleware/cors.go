16eb4a02bef457301e38f62d36589bb4c356376e
Akinakk <Artemi.Baburin@tptlive.ee>
2024-10-24 09:22:07 +0300

    update: Add DELETE and PUT methods to CORS

--- feedback-backend/middleware/cors.go
+++ feedback-backend/middleware/cors.go
@@ -7,7 +7,7 @@ import (
 func CORS(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Access-Control-Allow-Origin", "*")
-        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS")
         w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
         if r.Method == http.MethodOptions {
